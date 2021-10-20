2086ae6db41f3a59d5676a6caf0b59d77808e29f
gayoung <gayoung.ha@viewmagine.com>
2021-10-20 15:47:08 +0900

    updated get

--- app.py
+++ app.py
@@ -1,18 +1,38 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_restful import Api, Resource
 from flask_restful import reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
+
 class Login(Resource):
     def get(self):
-        params = request.get_json(force=True)
-        print(parmas)
+        
+        param_dict = request.args.to_dict()
+
+        id = param_dict['id']
+        
+        id_list = ["qlalf9824@naver.com", "gayoung5401@gmail.com", "asdfeg@viewmagine.com"]
+
+        if(id in id_list):
+            return {
+                'Response' : {
+                    'message' : 'This is a duplicate ID.'
+                }
+            }
+        else:
+            return {
+                'Response' : {
+                    'message' : 'This is the ID that you can use.'
+                }
+            }
+
+        
 
 
 
 api.add_resource(Login, '/login')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000, debug=True)
